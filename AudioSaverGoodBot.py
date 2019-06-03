f45bdba1659ee0af99f28edf4d244e0164611095
AleksandrStepa <by_step@list.ru>
2019-06-04 06:41:17 +1100

    add PostgreSQL support

--- AudioSaverGoodBot.py
+++ AudioSaverGoodBot.py
@@ -1,8 +1,17 @@
 import telebot
 from telebot.types import Message
+import psycopg2
 
+"""
+Telegram
+"""
 # запист в переменную токена полученного при регистрации бота
 TOKEN = '842176662:AAELOFzSczA_c62AhehiITxI1iAzWxyvK54'
+DB_NAME = 'dbn3od188bn1uk'
+USER = 'xegtezjpppdscr'
+PASSWORD = '0752bfc0afc8b03adf0147e874dee014f389636235f050f43f6488c68450e4e0'
+HOST = 'ec2-176-34-184-174.eu-west-1.compute.amazonaws.com'
+
 # создание экземпляра класса telebot
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
@@ -15,8 +24,28 @@ def bot_commands(message: Message):
 # методам экземпляра класса message
 def get_audio(message: Message):
     try:
-        # получение атрибута file_id (понадобится для метода download_file)
+        # создание экземпляра класса подклюсения с параметрами БД
+        con = psycopg2.connect(
+          database = DB_NAME,
+          user = USER,
+          password = PASSWORD,
+          host = HOST,
+          port="5432"
+        )
+        # получение атрибута file_id (понадобится для заполнения поля AUDIO)
         file_info = bot.get_file(message.voice.file_id)
+        voice = str(file_info.file_path)
+        # получение атрибута типа id типа User (понадобится для заполнения ID)
+        id = message.from_user.id
+        # создание объекта курсор, для работы с БД
+        cur = con.cursor()
+        # заполнение строки данными получеными ботом
+        cur.execute(
+        f"INSERT INTO audiosaver (ID, AUDIO) VALUES ({id},'{voice}')")
+        # обновление БД
+        con.commit()
+        # закрытие соединения с БД
+        con.close()
         # преобразование файла для его последующей загрузки
         downloaded_file = bot.download_file(file_info.file_path)
         # получение пути записи загруженного файла на диск
@@ -24,14 +53,14 @@ def get_audio(message: Message):
         # замена в пути символа '/' на символ '\' для ОС Windows
         #x = str('\\')
         path = path.replace('voice/','')
-        print (path)
         # открытие файла в режиме записи в двоичном виде
         with open (path, 'wb') as new_file:
             # запись файла
             new_file.write(downloaded_file)
         # ответ бота в случае успешной загрузки
-        bot.reply_to(message, 'загрузил')
+        bot.reply_to(message, f'Загрузил в БД сообщение {voice}')
     except Exception as erorr:
+        print(erorr)
         # если загрузка необходима на ПК с ОС Linux
         if erorr == FileNotFoundError:
             try:
